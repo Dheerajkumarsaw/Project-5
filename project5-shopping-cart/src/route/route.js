7818793279185d0458a444147f7fa1302c9e090a
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-24 18:43:14 +0530

    Post Api

--- project5-shopping-cart/src/route/route.js
+++ project5-shopping-cart/src/route/route.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controller/userController");
+
+
+router.post("/register", userController.createUser);
+
 
 module.exports = router
