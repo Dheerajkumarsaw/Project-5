f6ef39609c777523ff23276fb0638d0c44bf512e
Kaustubh Ganorkar <kaustubhganorkar@gmail.com>
2022-05-25 00:45:39 +0530

    getUser

--- project5-shopping-cart/src/controller/userController.js
+++ project5-shopping-cart/src/controller/userController.js
@@ -1,3 +1,26 @@
 const userModel = require("../model/userModel");
 const validator = require("../validator/validator")
 const bcrypt = require("bcrypt")
+
+const getUser = async (req, res)=>{
+    try {
+        const userId = req.params.userId
+    
+        if(!validator.isValidObjectId(userId)){
+            return res.status().send({status:false, message:"Invalid UserId"})
+        }
+
+        const isUserExist = await userModel.findById(userId)
+
+        if(!isUserExist){
+            return res.status(404).send({status:false, message:"User Not Found!"})
+        }
+
+        return res.status(200).send({status:true, data:isUserExist})
+        
+    } catch (error) {
+        res.status(500).send({error:error.message})
+    }
+}
+
+module.exports = {getUser}
--- project5-shopping-cart/src/route/route.js
+++ project5-shopping-cart/src/route/route.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controller/userController")
+
+router.get("/user/:userId/profile", userController.getUser)
 
 module.exports = router
