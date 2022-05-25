bbb72dfa12412f737d50994c6ac8aba974677bd5
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-25 23:55:24 +0530

    Few correction done on Schema

--- project5-shopping-cart/src/model/productModel.js
+++ project5-shopping-cart/src/model/productModel.js
@@ -4,24 +4,28 @@ const productSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        unique: false
+        unique: true,
+        trim: true
     },
     description: {
         type: String,
         required: true,
+        trim: true
     },
     price: {
         type: Number,
         required: true,
+        trim: true
     },
     currencyId: {
         type: String,
         required: true,
-        
+        trim: true
     },
     currencyFormat: {
         type: String,
         required: true,
+        trim: true
     },
     isFreeShipping: {
         type: Boolean,
@@ -34,11 +38,11 @@ const productSchema = new mongoose.Schema({
     style: {
         type: String
     },
-    availableSizes: {
-        type: [String],
+    availableSizes: [{
+        type: String,
         required: true,
         enum: ["S", "XS", "M", "X", "L", "XXL", "XL"]
-    },
+    }],
     installments: {
         type: Number
     },
